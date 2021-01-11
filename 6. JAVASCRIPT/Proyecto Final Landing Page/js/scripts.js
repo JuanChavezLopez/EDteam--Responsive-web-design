e7b47d16cefdc80a04447062f7d91f2ea4fc7815
Juan Chavez <jchavez666@gmail.com>
2021-01-11 17:52:32 +0200

    update

---
+++ 6. JAVASCRIPT/Proyecto Final Landing Page/js/scripts.js
@@ -0,0 +1,15 @@
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav          = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+
+// EJECUTAMOS EL SCRIPT
+
+showMenu('main-menu-toggle', 'main-nav')
